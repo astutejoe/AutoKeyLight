40fd9388308a252cb5f2462f0cff26a46a227930
Gabriel Garcia <gabriel.antunes.garcia@gmail.com>
2022-06-17 22:38:30 -0700

    Initial Commit

---
+++ AutoKeyLight/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace AutoKeyLight
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tmrCameraCheck = new System.Windows.Forms.Timer(this.components);
+            this.lblCameraState = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.lblLightState = new System.Windows.Forms.Label();
+            this.chkStartWithWindows = new System.Windows.Forms.CheckBox();
+            this.niTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrCameraCheck
+            // 
+            this.tmrCameraCheck.Enabled = true;
+            this.tmrCameraCheck.Interval = 1000;
+            this.tmrCameraCheck.Tick += new System.EventHandler(this.tmrCameraCheck_Tick);
+            // 
+            // lblCameraState
+            // 
+            this.lblCameraState.AutoSize = true;
+            this.lblCameraState.Location = new System.Drawing.Point(12, 56);
+            this.lblCameraState.Name = "lblCameraState";
+            this.lblCameraState.Size = new System.Drawing.Size(80, 15);
+            this.lblCameraState.TabIndex = 0;
+            this.lblCameraState.Text = "Camera is ON";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key Light IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(87, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(89, 23);
+            this.txtIP.TabIndex = 3;
+            this.txtIP.TextChanged += new System.EventHandler(this.txtIP_TextChanged);
+            // 
+            // lblLightState
+            // 
+            this.lblLightState.AutoSize = true;
+            this.lblLightState.Location = new System.Drawing.Point(12, 93);
+            this.lblLightState.Name = "lblLightState";
+            this.lblLightState.Size = new System.Drawing.Size(66, 15);
+            this.lblLightState.TabIndex = 4;
+            this.lblLightState.Text = "Light is ON";
+            // 
+            // chkStartWithWindows
+            // 
+            this.chkStartWithWindows.AutoSize = true;
+            this.chkStartWithWindows.Location = new System.Drawing.Point(12, 130);
+            this.chkStartWithWindows.Name = "chkStartWithWindows";
+            this.chkStartWithWindows.Size = new System.Drawing.Size(128, 19);
+            this.chkStartWithWindows.TabIndex = 5;
+            this.chkStartWithWindows.Text = "Start with Windows";
+            this.chkStartWithWindows.UseVisualStyleBackColor = true;
+            this.chkStartWithWindows.CheckedChanged += new System.EventHandler(this.chkStartWithWindows_CheckedChanged);
+            // 
+            // niTray
+            // 
+            this.niTray.ContextMenuStrip = this.cmsTray;
+            this.niTray.Icon = ((System.Drawing.Icon)(resources.GetObject("niTray.Icon")));
+            this.niTray.Text = "Auto Key Light";
+            this.niTray.Visible = true;
+            this.niTray.DoubleClick += new System.EventHandler(this.niTray_DoubleClick);
+            // 
+            // cmsTray
+            // 
+            this.cmsTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.tsmiExit});
+            this.cmsTray.Name = "cmsTray";
+            this.cmsTray.Size = new System.Drawing.Size(104, 48);
+            this.cmsTray.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.cmsTray_ItemClicked);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(103, 22);
+            this.tsmiOpen.Text = "Open";
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(103, 22);
+            this.tsmiExit.Text = "Exit";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.Controls.Add(this.chkStartWithWindows);
+            this.Controls.Add(this.lblLightState);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCameraState);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Auto Key Light";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.cmsTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCameraCheck;
+        private Label lblCameraState;
+        private Label label1;
+        private TextBox txtIP;
+        private Label lblLightState;
+        private CheckBox chkStartWithWindows;
+        private NotifyIcon niTray;
+        private ContextMenuStrip cmsTray;
+        private ToolStripMenuItem tsmiOpen;
+        private ToolStripMenuItem tsmiExit;
+    }
+}
---
+++ AutoKeyLight/MainForm.cs
@@ -0,0 +1,182 @@
+using Microsoft.Win32;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace AutoKeyLight
+{
+    public partial class MainForm : Form
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        const string webcamRegistryKey = @"Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam";
+        RegistryKey? startWithWindowsRegistry = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        bool isCameraOn = false;
+        string keylightURL = $"http://{Properties.Settings.Default.IP}:9123/elgato/lights";
+        CancellationTokenSource requestStateTokenSource;
+        CancellationToken requestStateToken;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            requestStateTokenSource = new CancellationTokenSource();
+            requestStateToken = requestStateTokenSource.Token;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RefreshCameraState();
+            tmrCameraCheck_Tick(null, null);
+            txtIP.Text = Properties.Settings.Default.IP;
+        }
+
+        private async void RefreshCameraState()
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(keylightURL, requestStateToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    JsonNode responseRoot = JsonNode.Parse(await  response.Content.ReadAsStringAsync(requestStateToken));
+                    isCameraOn = (responseRoot.AsObject()["lights"].AsArray()[0].AsObject()["on"].GetValue<int>() == 1);
+                    lblLightState.Text = isCameraOn ? "Light is ON" : "Light is OFF";
+                    lblLightState.ForeColor = isCameraOn ? Color.LawnGreen : Color.IndianRed;
+                }
+                else
+                {
+                    lblLightState.Text = "Error, check IP";
+                    lblLightState.ForeColor = Color.Red;
+                }
+            }
+            catch
+            {
+                lblLightState.Text = "Error, check IP";
+                lblLightState.ForeColor = Color.Red;
+
+                requestStateTokenSource = new CancellationTokenSource();
+                requestStateToken = requestStateTokenSource.Token;
+            }
+        }
+
+        private void CameraIsOn()
+        {
+            RefreshCameraState();
+
+            lblCameraState.Text = "Camera is ON";
+            lblCameraState.ForeColor = Color.LawnGreen;
+
+            if (!isCameraOn)
+            {
+                StringContent httpContent = new StringContent("{\"lights\": [{\"on\": 1}]}", Encoding.UTF8, "application/json");
+                httpClient.PutAsync(keylightURL, httpContent).Wait();
+            }
+        }
+
+        private void CameraIsOff()
+        {
+            RefreshCameraState();
+
+            lblCameraState.Text = "Camera is OFF";
+            lblCameraState.ForeColor = Color.IndianRed;
+
+            if (isCameraOn)
+            {
+                StringContent httpContent = new StringContent("{\"lights\": [{\"on\": 0}]}", Encoding.UTF8, "application/json");
+                httpClient.PutAsync(keylightURL, httpContent).Wait();
+            }
+        }
+
+        private void tmrCameraCheck_Tick(object sender, EventArgs e)
+        {
+            RegistryKey? webcamAppsKey = Registry.CurrentUser.OpenSubKey(webcamRegistryKey);
+
+            if (webcamAppsKey == null)
+                return;
+
+            string[] packagedKeys = webcamAppsKey.GetSubKeyNames();
+
+            foreach (string key in packagedKeys)
+            {
+                Int64? timestamp = (Int64?)webcamAppsKey.OpenSubKey(key).GetValue("LastUsedTimeStop");
+
+                if (timestamp != null && timestamp == 0)
+                {
+                    CameraIsOn();
+                    return;
+                }
+            }
+
+            RegistryKey? nonPackagedKey;
+            if ((nonPackagedKey = webcamAppsKey.OpenSubKey("NonPackaged")) != null)
+            {
+                string[] nonPackagedKeys = nonPackagedKey.GetSubKeyNames();
+
+                foreach (string key in nonPackagedKeys)
+                {
+                    Int64? timestamp = (Int64?)nonPackagedKey.OpenSubKey(key).GetValue("LastUsedTimeStop");
+
+                    if (timestamp != null && timestamp == 0)
+                    {
+                        CameraIsOn();
+                        return;
+                    }
+                }
+            }
+
+            CameraIsOff();
+        }
+
+        private void txtIP_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.IP = txtIP.Text;
+            Properties.Settings.Default.Save();
+
+            keylightURL = $"http://{txtIP.Text}:9123/elgato/lights";
+
+            requestStateTokenSource.Cancel();
+        }
+
+        private void chkStartWithWindows_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkStartWithWindows.Checked)
+                startWithWindowsRegistry.SetValue("MyApp", Application.ExecutablePath);
+            else
+                startWithWindowsRegistry.DeleteValue("MyApp", false);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                niTray.Visible = true;
+                this.Hide();
+                e.Cancel = true;
+            }
+        }
+
+        private void niTray_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            chkStartWithWindows.Checked = startWithWindowsRegistry.GetValue("MyApp") != null;
+
+            if (chkStartWithWindows.Checked)
+                this.Hide();
+        }
+
+        private void cmsTray_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Name)
+            {
+                case "tsmiExit":
+                    Application.Exit();
+                    break;
+                case "tsmiOpen":
+                    this.Show();
+                    break;
+            }
+        }
+    }
+}
